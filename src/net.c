c315784f819946018b5ce5cdddfab22ce61d8897
pos <pgousdal@users.noreply.github.com>
2026-09-13 18:41:01 +0200

    M7: implement multi-socket WaitSelect

--- src/net.c
+++ src/net.c
@@ -15,10 +15,7 @@ struct Library *SocketBase = 0;
 
 int ambot_net_open(void)
 {
-    if (SocketBase != 0) {
-        return 0;
-    }
-
+    if (SocketBase != 0) return 0;
     SocketBase = OpenLibrary("bsdsocket.library", 0);
     return SocketBase != 0 ? 0 : -1;
 }
@@ -38,14 +35,11 @@ int ambot_net_connect_ipv4(const char *host, unsigned short port)
     int sock;
 
     resolved = gethostbyname(host);
-    if (resolved == 0 || resolved->h_addr_list == 0 || resolved->h_addr_list[0] == 0) {
+    if (resolved == 0 || resolved->h_addr_list == 0 || resolved->h_addr_list[0] == 0)
         return -1;
-    }
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        return -1;
-    }
+    if (sock < 0) return -1;
 
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
@@ -56,22 +50,17 @@ int ambot_net_connect_ipv4(const char *host, unsigned short port)
         CloseSocket(sock);
         return -1;
     }
-
     return sock;
 }
 
 int ambot_net_send_all(int sock, const char *data, unsigned int length)
 {
     unsigned int sent = 0;
-
     while (sent < length) {
         int rc = send(sock, data + sent, length - sent, 0);
-        if (rc <= 0) {
-            return -1;
-        }
+        if (rc <= 0) return -1;
         sent += (unsigned int)rc;
     }
-
     return 0;
 }
 
@@ -82,22 +71,46 @@ int ambot_net_recv(int sock, char *buffer, unsigned int length)
 
 int ambot_net_wait(int sock, unsigned long signal_mask, unsigned long *signals)
 {
+    unsigned long ready = 0;
+    int socks[1];
+    socks[0] = sock;
+    if (ambot_net_wait_many(socks, 1, signal_mask, signals, &ready) < 0) return -1;
+    return (ready & 1UL) != 0 ? 1 : 0;
+}
+
+int ambot_net_wait_many(const int *socks,
+                        unsigned int count,
+                        unsigned long signal_mask,
+                        unsigned long *signals,
+                        unsigned long *ready_mask)
+{
     fd_set readfds;
     ULONG signal_bits = (ULONG)signal_mask;
+    int maxfd = -1;
     int rc;
+    unsigned int i;
+    unsigned long ready = 0;
 
     FD_ZERO(&readfds);
-    FD_SET(sock, &readfds);
+    for (i = 0; i < count; ++i) {
+        if (socks[i] >= 0) {
+            FD_SET(socks[i], &readfds);
+            if (socks[i] > maxfd) maxfd = socks[i];
+        }
+    }
 
-    rc = WaitSelect(sock + 1, &readfds, 0, 0, 0, &signal_bits);
+    rc = WaitSelect(maxfd + 1, &readfds, 0, 0, 0, &signal_bits);
     if (signals != 0) *signals = (unsigned long)signal_bits;
     if (rc < 0) return -1;
-    return FD_ISSET(sock, &readfds) ? 1 : 0;
+
+    for (i = 0; i < count && i < sizeof(unsigned long) * 8U; ++i)
+        if (socks[i] >= 0 && FD_ISSET(socks[i], &readfds)) ready |= 1UL << i;
+
+    if (ready_mask != 0) *ready_mask = ready;
+    return rc;
 }
 
 void ambot_net_close_socket(int sock)
 {
-    if (sock >= 0) {
-        CloseSocket(sock);
-    }
+    if (sock >= 0) CloseSocket(sock);
 }
