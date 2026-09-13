b8f1b353d549438fb930ecd8fb8103eb006e4227
pos <pgousdal@users.noreply.github.com>
2026-09-13 18:40:51 +0200

    M7: add multi-socket wait API

--- include/net.h
+++ include/net.h
@@ -7,6 +7,11 @@ int ambot_net_connect_ipv4(const char *host, unsigned short port);
 int ambot_net_send_all(int sock, const char *data, unsigned int length);
 int ambot_net_recv(int sock, char *buffer, unsigned int length);
 int ambot_net_wait(int sock, unsigned long signal_mask, unsigned long *signals);
+int ambot_net_wait_many(const int *socks,
+                        unsigned int count,
+                        unsigned long signal_mask,
+                        unsigned long *signals,
+                        unsigned long *ready_mask);
 void ambot_net_close_socket(int sock);
 
 #endif
