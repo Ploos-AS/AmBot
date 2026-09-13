929ffbf0f94e6243c448394911e2b8155102e2ea
pos <pgousdal@users.noreply.github.com>
2026-09-13 18:34:15 +0200

    M6: accept optional config file

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 
 static void usage(const char *program)
 {
-    printf("Usage: %s HOST PORT NICK [USER] [PASS] [OWNER]\n", program);
-    puts("Example: AmBot irc.libera.chat 6667 AmBot ambot secret MyNick");
+    printf("Usage: %s HOST PORT NICK [USER] [PASS] [OWNER] [CONFIG]\n", program);
+    puts("Example: AmBot irc.libera.chat 6667 AmBot ambot secret MyNick PROGDIR:AmBot.cfg");
 }
 
 int ambot_run(int argc, char **argv)
@@ -18,7 +18,7 @@ int ambot_run(int argc, char **argv)
     puts(AMBOT_NAME " " AMBOT_VERSION);
     puts("ARexx port reserved: " AMBOT_REXX_PORT);
 
-    if (argc < 4 || argc > 7) {
+    if (argc < 4 || argc > 8) {
         usage(argv[0]);
         return 10;
     }
@@ -35,6 +35,7 @@ int ambot_run(int argc, char **argv)
     config.user = argc >= 5 ? argv[4] : argv[3];
     config.pass = argc >= 6 ? argv[5] : 0;
     config.owner_nick = argc >= 7 ? argv[6] : 0;
+    config.config_path = argc >= 8 ? argv[7] : 0;
 
     return ambot_session_run(&config);
 }
