5e6c7a02d249afb030df20ee89f2b34c0aa2aa24
pos <pgousdal@users.noreply.github.com>
2026-09-13 18:27:46 +0200

    M5: make hook runner service AMBOT callbacks

--- include/rexx.h
+++ include/rexx.h
@@ -12,7 +12,9 @@ int ambot_rexx_open(struct ambot_rexx *rexx);
 void ambot_rexx_close(struct ambot_rexx *rexx);
 unsigned long ambot_rexx_signal_mask(const struct ambot_rexx *rexx);
 void ambot_rexx_process(struct ambot_rexx *rexx, struct ambot_control *control);
-int ambot_rexx_run_script(const char *script_path,
+int ambot_rexx_run_script(struct ambot_rexx *rexx,
+                          struct ambot_control *control,
+                          const char *script_path,
                           const char *event_name,
                           const char *nick,
                           const char *target,
