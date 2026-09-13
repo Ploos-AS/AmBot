ec8800f59e03d3e05d8a7d35bdb460f1e06f807f
pos <pgousdal@users.noreply.github.com>
2026-09-13 18:42:17 +0200

    M7: bump version

--- include/ambot.h
+++ include/ambot.h
@@ -2,7 +2,7 @@
 #define AMBOT_H
 
 #define AMBOT_NAME "AmBot"
-#define AMBOT_VERSION "0.6.0-m6"
+#define AMBOT_VERSION "0.7.0-m7"
 #define AMBOT_REXX_PORT "AMBOT"
 
 int ambot_run(int argc, char **argv);
