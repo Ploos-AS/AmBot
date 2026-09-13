bc7e0725031928c0ede23066d1a4142c0e888f6c
pos <pgousdal@users.noreply.github.com>
2026-09-13 18:28:20 +0200

    M5: bind hook context to ARexx control

--- include/hooks.h
+++ include/hooks.h
@@ -5,13 +5,20 @@
 
 #define AMBOT_HOOK_SCRIPT_DIR "REXX:AmBot"
 
+struct ambot_rexx;
+struct ambot_control;
+
 struct ambot_hook_context {
     const char *script_dir;
+    struct ambot_rexx *rexx;
+    struct ambot_control *control;
     unsigned long invoked;
     unsigned long failed;
 };
 
-void ambot_hooks_init(struct ambot_hook_context *context);
+void ambot_hooks_init(struct ambot_hook_context *context,
+                      struct ambot_rexx *rexx,
+                      struct ambot_control *control);
 void ambot_hooks_dispatch_event(const struct ambot_event *event, void *userdata);
 
 #endif
