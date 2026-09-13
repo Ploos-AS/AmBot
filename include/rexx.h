3ed1344e8d94b5d0846842149605ca9b3fa43726
pos <pgousdal@users.noreply.github.com>
2026-09-13 18:26:39 +0200

    M5: expose RexxMast script runner

--- include/rexx.h
+++ include/rexx.h
@@ -12,5 +12,10 @@ int ambot_rexx_open(struct ambot_rexx *rexx);
 void ambot_rexx_close(struct ambot_rexx *rexx);
 unsigned long ambot_rexx_signal_mask(const struct ambot_rexx *rexx);
 void ambot_rexx_process(struct ambot_rexx *rexx, struct ambot_control *control);
+int ambot_rexx_run_script(const char *script_path,
+                          const char *event_name,
+                          const char *nick,
+                          const char *target,
+                          const char *text);
 
 #endif
